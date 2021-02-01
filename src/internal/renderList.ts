2dcef6b3381f57556748f69d5546ceba755a8c34
Raghav Misra <raghav.m2014@gmail.com>
2021-02-01 15:11:46 -0800

    Okay so deep nesting in lists works now :D

--- src/internal/renderList.ts
+++ src/internal/renderList.ts
@@ -1,6 +1,5 @@
 import { watchful } from "../reactivity/watchful";
 import { Model } from "../dom/Model";
-import { list } from "../reactivity/list";
 import { getNodeAttributes } from "./getNodeAttributes";
 
 export interface ITemplateData {
@@ -10,44 +9,38 @@ export interface ITemplateData {
 }
 
 export function renderList<T extends object>(model: Model<T>, template: HTMLTemplateElement, templateData: ITemplateData) {
+    // Create the parent of nodes:
     const listContainer = document.createElement("div");
     template.parentNode?.insertBefore(listContainer, template);
 
+    // Add all spec-compliant attributes:
     Object.entries(getNodeAttributes(template)).forEach(([key, value]) => {
         if (!key.startsWith("r-")) {
             listContainer.setAttribute(key, value);
         }
     });
 
-    // Convert the model into a bunch of computed functions to use in the underlyingModels:
+    // Convert the model into a bunch of computed functions to use in the underlying models:
     const modelComputedCopy = Object.create(null);
     Object.keys(model.data).forEach(key => modelComputedCopy[key] = () => model.data[key as keyof T]);
 
+    // Create clones:
+    for (let i = 0; i < (model.data[templateData.list as keyof T] as any).length; i++) {
+        const clone = (template.content.firstElementChild as Node).cloneNode(true) as HTMLElement;
+        listContainer.appendChild(clone);
 
-    (model.data[templateData.list as keyof T] as unknown as any[]).forEach((item, index) => watchful(() => {
+        // Use a watcher to autoupdate the
+        watchful(() => {
+            new Model({ 
+                data: {
+                    ...modelComputedCopy,
+                    // Dynamic property, so that nested values update without full renders:
+                    [templateData.item]: () => (model.data[templateData.list as keyof T] as any)[i],
+                    [templateData.index]: i
+                },
 
-        const underlyingModel = new Model({ 
-            data: {
-                ...modelComputedCopy,
-                [templateData.item]: (model.data[templateData.list as keyof T] as any)[index], // use this over the item parameter for reactivity
-                [templateData.index]: index
-            },
-
-            methods: {}
+                methods: {}
+            }).mount(clone);
         });
-
-        const clone = template.content.firstElementChild && template.content.firstElementChild.cloneNode(true);
-
-        if (clone) {
-            if (listContainer.childNodes[index]) {
-                listContainer.replaceChild(clone, listContainer.childNodes[index]);
-            }
-
-            else {
-                listContainer.appendChild(clone);
-            }
-
-            underlyingModel.mount(clone as HTMLElement);
-        }
-    }));
+    }
 }
